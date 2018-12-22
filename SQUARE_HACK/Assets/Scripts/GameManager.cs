fd42000b91edc7288c72eab6299d0183eadff37c
Morimoto Keigo <keigo7624@gmail.com>
2018-12-22 15:31:08 +0900

    UnityProject追加

---
+++ SQUARE_HACK/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+	
+	void Awake(){
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			//このオブジェクトを破壊する
+			Destroy(gameObject);
+		}
+
+		InitGame();
+		
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void InitGame(){
+
+	}
+}
