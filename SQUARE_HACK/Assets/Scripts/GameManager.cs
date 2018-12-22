61715fbe3291c372ea1945a45a8c34fd1aacf352
Morimoto Keigo <keigo7624@gmail.com>
2018-12-22 17:41:31 +0900

    GetCurrentFrame関数などの追加

--- SQUARE_HACK/Assets/Scripts/GameManager.cs
+++ SQUARE_HACK/Assets/Scripts/GameManager.cs
@@ -6,6 +6,15 @@ public class GameManager : MonoBehaviour {
 
 	public static GameManager instance = null;
 	
+	//現在のFrameTime
+	public static float CurrentFrame;
+
+
+
+	[SerializeField]
+	public Vector3 LavaPositon;
+	public Vector3 PlayerPosition; 
+	
 	void Awake(){
 		if (instance == null) {
 			instance = this;
@@ -26,10 +35,34 @@ public class GameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		CalcCurrentFrame();
+		Debug.Log(GetCurrentFrameTime());
+		CheckHitLava();
 	}
 
 	void InitGame(){
-
+		CurrentFrame = 0;
+		LavaPositon = new Vector3(0,0,0);
+		PlayerPosition = new Vector3(0,0,0);
 	}
+
+	void CalcCurrentFrame(){
+		CurrentFrame += Time.deltaTime;
+	}
+	void GameOver(){
+		Debug.Log("testGameOver");
+	}
+
+	float GetCurrentFrameTime(){
+		return CurrentFrame;
+	}
+
+	void CheckHitLava(){
+		var distance = PlayerPosition.y - LavaPositon.y;
+		if(distance <= 0){
+			Debug.Log("testCheckHitLava");
+			GameOver();
+		}
+	}
+	
 }
