b8093ca8c36698095e0b07f8d13f129c62369d99
ishidashota <c611621050@tokushima-u.ac.jp>
2018-12-24 12:58:11 +0900

    ゲージでスピード変化,プレイヤープレハブアプライ

--- SQUARE_HACK/Assets/GaugeController.cs
+++ SQUARE_HACK/Assets/GaugeController.cs
@@ -22,14 +22,19 @@ public class GaugeController : MonoBehaviour {
 		if(playerController.justGauge >= 0 && playerController.justGauge <= 100)
 		{
 			one.value = playerController.justGauge;
+			playerController.speed = 1.5;
 		}
 		else if(playerController.justGauge >= 100 && playerController.justGauge <= 200)
 		{
 			two.value = playerController.justGauge;
+			playerController.speed = 2.0;
+
 		}
 		else if(playerController.justGauge >= 200)
 		{
 			three.value = playerController.justGauge;
+			playerController.speed = 2.5;
+
 		}
 	}
 }
