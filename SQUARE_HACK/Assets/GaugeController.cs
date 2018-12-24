e0fef1aa2c2ceb3705385304607620167ab694ca
ishidashota <c611621050@tokushima-u.ac.jp>
2018-12-24 13:02:50 +0900

    フロート変換してバグ修正

--- SQUARE_HACK/Assets/GaugeController.cs
+++ SQUARE_HACK/Assets/GaugeController.cs
@@ -22,18 +22,18 @@ public class GaugeController : MonoBehaviour {
 		if(playerController.justGauge >= 0 && playerController.justGauge <= 100)
 		{
 			one.value = playerController.justGauge;
-			playerController.speed = 1.5;
+			playerController.speed = 20f;
 		}
 		else if(playerController.justGauge >= 100 && playerController.justGauge <= 200)
 		{
 			two.value = playerController.justGauge;
-			playerController.speed = 2.0;
+			playerController.speed = 20f * 1.5f;
 
 		}
 		else if(playerController.justGauge >= 200)
 		{
 			three.value = playerController.justGauge;
-			playerController.speed = 2.5;
+			playerController.speed = 20f * 2.0f;
 
 		}
 	}
