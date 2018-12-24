bf0e10a2b803b9d394f8bb03e5da0c0b1f3ff22d
ishidashota <c611621050@tokushima-u.ac.jp>
2018-12-25 01:31:06 +0900

    ゲージ修正、UI描画、時間を画像に差し替え

--- SQUARE_HACK/Assets/Scripts/TimeController.cs
+++ SQUARE_HACK/Assets/Scripts/TimeController.cs
@@ -1,12 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TimeController : MonoBehaviour {
+	public List<Sprite> timeSprite;
 	public float nowTime;
 	public float startTime;
 	public Text timeText;
+	public List<Image> timeImage;
 	// Use this for initialization
 	void Awake() {
 		timeText = GetComponent<Text>();
@@ -24,11 +27,18 @@ public class TimeController : MonoBehaviour {
 		int minute = (int)(diffTime / 60f);
 		int second = (int)(diffTime % 60f);
 		var millis = diffTime % 1;
-		var millisStr = millis.ToString().Remove(0,2).Remove(3);
-		if(second / 10 == 0)
-			timeText.text = minute + ":0" + second + "." + millisStr;
-		else
-			timeText.text = minute + ":" + second + "." + millisStr;
+		var millisStr = millis.ToString().Remove(0,2).Remove(2);
+		timeImage[0].sprite = timeSprite[minute / 10];
+		timeImage[1].sprite = timeSprite[minute % 10];
+		timeImage[2].sprite = timeSprite[second / 10];
+		timeImage[3].sprite = timeSprite[second % 10];
+		timeImage[4].sprite = timeSprite[Int16.Parse(millisStr.Substring(0,1))];
+		timeImage[5].sprite = timeSprite[Int16.Parse(millisStr.Substring(1,1))];
+		
+		// if(second / 10 == 0)
+		// 	timeText.text = minute + ":0" + second + "." + millisStr;
+		// else
+		// 	timeText.text = minute + ":" + second + "." + millisStr;
 
 	}
 }
