e8e55999c2320e0e6d19adc7ffa51be4860c7dab
Morimoto Keigo <keigo7624@gmail.com>
2018-12-25 09:26:18 +0900

    ResultBGM追加

--- SQUARE_HACK/Assets/Scripts/ResultManager.cs
+++ SQUARE_HACK/Assets/Scripts/ResultManager.cs
@@ -2,8 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class ResultManager : MonoBehaviour {
 
+
+	[SerializeField]
+	private float rankS;
+	[SerializeField]
+	private float rankA;
+	[SerializeField]
+	private float rankB;
+	[SerializeField]
+	private float rankC;
 
 	[SerializeField]
 	private AudioClip BGM;
@@ -11,27 +21,43 @@ public class ResultManager : MonoBehaviour {
 	[SerializeField]
 	private AudioClip SE;
 	
+	public Text RankText;
 
-
+	public float Score;
 	// Use this for initialization
 	void Start () {
-		SoundManager.instance.PlaySingleSound(BGM);	
-		Debug.Log("Result Time" + TimeToResult.instance.GetComponent<TimeToResult>().GetRecordTime());
+		//SoundManager.instance.PlaySingleSound(BGM);	
+		Score = TimeToResult.instance.GetRecordTime();
+		//Debug.Log("Result Time" + TimeToResult.instance.GetComponent<TimeToResult>().GetRecordTime());
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		
 		
 		if(Input.GetKeyDown(KeyCode.Space)){
 			ChangeScene();
 		}
 		
-
+		displayRank();
 
 	}
 
 	void ChangeScene(){
 		SoundManager.instance.PlaySoundSE(SE);
 		SceneManager.LoadScene("GAME");
+	}
+
+	void displayRank(){
+		if(Score < rankC){
+			RankText.GetComponent<Text>().text = "rankC";
+		}else if(Score < rankB){
+			RankText.GetComponent<Text>().text = "rankB";
+		}else if(Score < rankA){
+			RankText.GetComponent<Text>().text = "rankA";
+		}else if(Score < rankS){
+			RankText.GetComponent<Text>().text = "rankS";
+		}
 	}
 }
